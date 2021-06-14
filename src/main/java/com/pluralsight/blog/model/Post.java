646f4bd1b011745b5f17cec149097a3d2f79adb3
Kuppa <gaurav.kuppa@capgemini.com>
2021-06-14 13:55:25 -0700

    add author relationships and data

--- src/main/java/com/pluralsight/blog/model/Post.java
+++ src/main/java/com/pluralsight/blog/model/Post.java
@@ -24,8 +24,11 @@ public class Post {
     @Temporal(TemporalType.DATE)
     @CreationTimestamp
     private Date date;
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Author author;
 
     public Post() {
+
         super();
     }
 
@@ -72,6 +75,8 @@ public class Post {
         this.date = date;
     }
 
+    public Author getAuthor() { return author; }
+    public void setAuthor(Author author) { this.author = author; }
     @Override
     public boolean equals(Object obj) {
         if (!(obj instanceof Post))
